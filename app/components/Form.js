9d6278bb496f0ece006838b78958305c243941fa
jesteen1 <tamilbillons@gmail.com>
2025-12-24 08:22:44 +0530

    update project updateded

--- app/components/Form.js
+++ app/components/Form.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
-function Form({ senddata ,seriesname=[] }) {
-    
+function Form({ senddata, seriesname = [] }) {
+
     const [EpisodeName, SetEpisodeName] = useState("episode 1".toUpperCase())
     const [movieurl, Setmovieurl] = useState("")
     const [Year, SetYear] = useState("")
@@ -40,60 +40,60 @@ function Form({ senddata ,seriesname=[] }) {
                             Series Name
                         </label>
 
-                        {Type=="File"? 
-                         <select 
-                value={seriesnames}
-                onChange={(e)=>{SetSeriesname(e.target.value)}}
-                className="bg-gray-800 px-4 py-2 rounded-lg border-2 border-gray-300  text-white 
+                        {Type == "File" ?
+                            <select
+                                value={seriesnames}
+                                onChange={(e) => { SetSeriesname(e.target.value) }}
+                                className="bg-gray-800 px-4 py-2 rounded-lg border-2 border-gray-300  text-white 
                    text-sm font-medium shadow-sm hover:border-blue-400 focus:outline-none 
                    focus:ring-2 focus:ring-blue-500 focus:border-transparent 
                    transition-all duration-200 cursor-pointer
                    min-w-[150px] max-w-[200px]">
 
-               
-                {isArray ? (
-                    seriesname.map((item, index) => (
-                        <option  key={index} value={item}>
-                            {item}
-                        </option>
-                    ))
-                ) : (
-                    <option value={seriesnames}>{seriesnames}</option>
-                )}
 
-            </select>:<input type="text" id="names" name="names"
-                            className="all-caps w-full px-4 py-3 bg-gray-800 border border-gray-700 rounded-xl text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 placeholder-gray-500"
-                            placeholder="Enter series name" required value={seriesnames} onChange={(e) => SetSeriesname(e.target.value.toUpperCase())} />}
+                                {isArray ? (
+                                    seriesname.map((item, index) => (
+                                        <option key={index} value={item}>
+                                            {item}
+                                        </option>
+                                    ))
+                                ) : (
+                                    <option value={seriesnames}>{seriesnames}</option>
+                                )}
+
+                            </select> : <input type="text" id="names" name="names"
+                                className="all-caps w-full px-4 py-3 bg-gray-800 border border-gray-700 rounded-xl text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 placeholder-gray-500"
+                                placeholder="Enter series name" required value={seriesnames} onChange={(e) => SetSeriesname(e.target.value.toUpperCase())} />}
                     </div>
                     <div>
                         <label className="block text-sm font-medium text-white mb-1" htmlFor="name">
-                          {Type=="Folder"?"description":"Name of episode"}  
+                            {Type == "Folder" ? "description" : "Name of episode"}
                         </label>
 
                         <input type="text" id="names" name="names"
                             className="all-caps  w-full px-4 py-3 bg-gray-800 border border-gray-700 rounded-xl text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 placeholder-gray-500"
-                            placeholder= {Type=="Folder"?" enter the description":"enter  the episode name"}   required value={EpisodeName} onChange={(e) => SetEpisodeName(e.target.value.toUpperCase())} />
+                            placeholder={Type == "Folder" ? " enter the description" : "enter  the episode name"} required value={EpisodeName} onChange={(e) => SetEpisodeName(e.target.value.toUpperCase())} />
                     </div>
-                     {Type=="Folder"?null:   <div>
+                    {Type == "Folder" ? null : <div>
 
                         <label className="block text-sm font-medium text-white mb-1" htmlFor="name">
                             Name of season
                         </label>
-  
+
                         <input type="text" id="names" name="names"
                             className="all-caps  w-full px-4 py-3 bg-gray-800 border border-gray-700 rounded-xl text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 placeholder-gray-500"
                             placeholder="Enter season name" required value={season} onChange={(e) => SetSeason(e.target.value.toUpperCase())} />
                     </div>}
-                  
 
-                    
-                   
-                            <div>
+
+
+
+                    <div>
                         <label className="block text-sm font-medium text-white mb-1" htmlFor="year">
                             Release Year
                         </label>
                         <input type="number" id="year" name="year"
-                            className="all-caps  w-full px-4 py-3 bg-gray-800 border border-gray-700 rounded-xl text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200"
+                            className="  w-full px-4 py-3 bg-gray-800 border border-gray-700 rounded-xl text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200"
                             placeholder="e.g. 2024" required value={Year} onChange={(e) => SetYear(e.target.value)} />
                     </div>
 
@@ -102,18 +102,18 @@ function Form({ senddata ,seriesname=[] }) {
                             Cover Image URL
                         </label>
                         <input type="url" id="imageUrl" name="imageUrl"
-                            className="all-caps  w-full px-4 py-3 bg-gray-800 border border-gray-700 rounded-xl text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200"
+                            className="  w-full px-4 py-3 bg-gray-800 border border-gray-700 rounded-xl text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200"
                             placeholder="https://example.com/image.jpg" required value={Imageurl} onChange={(e) => SetImageurl(e.target.value)} />
                     </div>
-                    {Type=="Folder"?null: <div>
+                    {Type == "Folder" ? null : <div>
                         <label className="block text-sm font-medium text-white mb-1" htmlFor="movieurl">
                             Cover movie URL
                         </label>
                         <input type="url" id="imageUrl" name="imageUrl"
-                            className="all-caps w-full px-4 py-3 bg-gray-800 border border-gray-700 rounded-xl text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200"
-                            placeholder="movie url" required value={movieurl} onChange={(e) => Setmovieurl(e.target.value)} />
-                    </div> }
-                  
+                            className=" w-full px-4 py-3 bg-gray-800 border border-gray-700 rounded-xl text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200"
+                            placeholder="movie url" value={movieurl} onChange={(e) => Setmovieurl(e.target.value)} />
+                    </div>}
+
                     <p className=" text-white">file type</p>
                     <select name="" id="" className="bg-gray-800 border  focus-within::bg-gray-800 text-white  m-5 p-5 m-5 focus:outline-none focus:border-transparent transition duration-200" value={Type} onChange={(e) => SetType(e.target.value)}>
                         <option value="" className="bg-gray-800 focus::bg-gray-800" >.....</option>
